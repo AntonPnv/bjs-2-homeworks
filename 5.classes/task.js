ec8f8bb6d3424e4ed5c520393399e8f01356a47c
AntonPnv <anton.panov992@gmail.com>
2023-07-21 14:58:50 +0400

    Пятое задание полностью готово

---
+++ 5.classes/task.js
@@ -0,0 +1,201 @@
+"use strict";
+// Задача № 1
+
+class PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		this.name = name;
+		this.releaseDate = releaseDate;
+		this.pagesCount = pagesCount;
+		this._state = 100;
+		this.type = null;
+	}
+
+	fix() {
+		this.state = this.state * 1.5;
+		if (this.state > 100) {
+			this.state = 100;
+		}
+	}
+
+	set state(value) {
+		if (value < 0) {
+			this._state = 0;
+		}
+		if (value > 100) {
+			this._state = 100;
+		} else {
+			this._state = value;
+		}
+	}
+
+	get state() {
+		return this._state;
+	}
+}
+
+class Magazine extends PrintEditionItem {
+	constructor(name, releadeDate, pagesCount) {
+		super(name, releadeDate, pagesCount);
+		this.type = 'magazine';
+	}
+}
+
+class Book extends PrintEditionItem {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.type = 'book';
+		this.author = author;
+	}
+}
+
+class NovelBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = 'novel';
+	}
+}
+
+class FantasticBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = 'fantastic';
+	}
+}
+
+class DetectiveBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = 'detective';
+	}
+}
+
+// Пример использования:
+const sherlock = new PrintEditionItem(
+	"Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+	2019,
+	1008
+);
+
+console.log(sherlock.releaseDate); //2019
+console.log(sherlock.state); //100
+sherlock.fix();
+console.log(sherlock.state); //100
+
+const picknick = new FantasticBook(
+	"Аркадий и Борис Стругацкие",
+	"Пикник на обочине",
+	1972,
+	168
+);
+
+console.log(picknick.author); //"Аркадий и Борис Стругацкие"
+picknick.state = 10;
+console.log(picknick.state); //10
+picknick.fix();
+console.log(picknick.state);
+
+// Задача № 2
+
+class Library {
+	constructor(name) {
+		this.name = name;
+		this.books = [];
+	}
+
+	addBook(book) {
+		if (book.state > 30) {
+			this.books.push(book);
+		}
+	}
+
+	findBookBy(type, value) {
+		const foundBook = this.books.find((book) => book[type] === value);
+		return foundBook || null;
+	}
+
+	giveBookByName(bookName) {
+		const index = this.books.findIndex((book) => book.name === bookName);
+		if (index !== -1) {
+			const book = this.books.splice(index, 1)[0];
+			return book;
+		}
+		return null;
+	}
+}
+
+// Пример использования:
+const library = new Library("Библиотека имени Ленина");
+
+library.addBook(
+	new DetectiveBook(
+		"Артур Конан Дойл",
+		"Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+		2019,
+		1008
+	)
+);
+library.addBook(
+	new FantasticBook(
+		"Аркадий и Борис Стругацкие",
+		"Пикник на обочине",
+		1972,
+		168
+	)
+);
+library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+library.addBook(new Magazine("Мурзилка", 1924, 60));
+
+console.log(library.findBookBy("name", "Властелин колец")); //null
+console.log(library.findBookBy("releaseDate", 1924).name); //"Мурзилка"
+
+console.log("Количество книг до выдачи: " + library.books.length); //Количество книг до выдачи: 4
+library.giveBookByName("Машина времени");
+console.log("Количество книг после выдачи: " + library.books.length); //Количество книг после выдачи: 3
+
+// Задача № 3 **
+
+class Student {
+	constructor(name) {
+		this.name = name;
+		this.marks = {};
+	}
+
+	addMark(mark, subject) {
+		if (mark < 2 || mark > 5) {
+			return;
+		}
+
+		if (!this.marks[subject]) {
+			this.marks[subject] = [];
+		}
+
+		this.marks[subject].push(mark);
+	}
+
+	getAverageBySubject(subject) {
+		if (!this.marks[subject] || this.marks[subject].length === 0) {
+			return 0;
+		}
+
+		const sum = this.marks[subject].reduce((total, mark) => total + mark, 0);
+		return sum / this.marks[subject].length;
+	}
+
+	getAverage() {
+		const subjects = Object.keys(this.marks);
+		const totalSum = subjects.reduce((total, subject) => total + this.getAverageBySubject(subject), 0);
+		return totalSum / subjects.length;
+	}
+}
+
+// Пример использования:
+const student = new Student("Олег Никифоров");
+student.addMark(5, "химия");
+student.addMark(5, "химия");
+student.addMark(5, "физика");
+student.addMark(4, "физика");
+student.addMark(6, "физика"); // Оценка не добавится, так как больше 5
+
+console.log("Средний балл по предмету физика:", student.getAverageBySubject("физика")); // Средний балл по предмету физика 4.5
+console.log("Средний балл по предмету биология:", student.getAverageBySubject("биология")); // Вернёт 0, так как по такому предмету нет никаких оценок.
+console.log("Средний балл по всем предметам:", student.getAverage()); // Средний балл по всем предметам 4.75
